6586bde44e87593042d00681b1c2e00b333c8408
yuwei-cao-git <yuwei.cao94@gmail.com>
2025-09-23 23:10:02 -0700

    cfg

--- train.sh
+++ train.sh
@@ -1,90 +1,22 @@
 #!/bin/bash
 
-LOG_FILE="log.txt"
+LOG_FILE="log_1.txt"
 SLEEP_DURATION=10  # seconds
 
 echo "Starting training..." | tee -a "$LOG_FILE"
 
 # List of commands to run
 commands=(
-<<<<<<< HEAD
-    # no_nomal
-    # pre-training: rmf_sp
-    "python train_fuse.py --data_dir '/mnt/g/rmf/rmf_superpixel_dataset' --config 'configs/config_rmf_sp.yaml' --task 'tsc_aligned' --log_name 'tsc_aligned_rmf_sp' --gpus 1 --batch_size 32 --head 'fuse_head' --dataset 'rmf_sp' --encoder 'b' --network 'ResUnet' --lr 1e-3 --emb_dims 768 --loss_func 'mse' --scheduler 'steplr'"
+    "python train_fuse.py --batch_size 64 --gpu 1 --config "./configs/config_ovf_coarser.yaml" --data_dir '/mnt/g/ovf/ovf_superpixel_dataset' --lr 1e-3 --optimizer 'adam' --loss_func "wmse" --dataset "ovf_coarser" --head "fuse_head" --task "tsc" --network 'ResUnet' --scheduler 'steplr' --log_name "tsc_wmse_ovf_coaser_pretraining""
 
-    # training from scratch: ovf_sp
-    "python train_fuse.py --data_dir '/mnt/g/ovf/ovf_superpixel_dataset' --config 'configs/config_ovf_sp.yaml' --task 'tsc_aligned' --log_name 'tsc_aligned_ovf_sp' --gpus 1 --batch_size 32 --head 'fuse_head' --dataset 'ovf_sp' --encoder 'b' --network 'ResUnet' --lr 5e-4 --emb_dims 768 --loss_func 'mse' --scheduler 'steplr'"
+    "python train_fuse.py --batch_size 64 --gpu 1 --config "./configs/config_ovf_coarser.yaml" --data_dir '/mnt/g/ovf/ovf_superpixel_dataset' --lr 1e-3 --optimizer 'adam' --loss_func "ewmse" --dataset "ovf_coarser" --head "fuse_head" --task "tsc" --network 'ResUnet' --scheduler 'steplr' --log_name "tsc_ewmse_ovf_coaser_pretraining""
+    
+    "python finetune.py --cfg 'configs/finetune.yaml' --ft_mode 'freeze_last_1' --pretrained_ckpt 'tl_logs/tsc_aligned_rmf_sp/checkpoints/final_model.ckpt'"
+    
+    "python finetune.py --cfg 'configs/finetune.yaml' --ft_mode 'freeze_last_2' --pretrained_ckpt 'tl_logs/tsc_aligned_rmf_sp/checkpoints/final_model.ckpt'"
 
-    # training from scratch: ovf_genus
-    "python train_fuse.py --data_dir '/mnt/g/ovf/ovf_superpixel_dataset' --config 'configs/config_ovf_genus.yaml' --task 'tsc_aligned' --log_name 'tsc_aligned_ovf_genus' --gpus 1 --batch_size 32 --head 'fuse_head' --dataset 'ovf_genus' --encoder 'b' --network 'ResUnet' --lr 5e-4 --emb_dims 768 --loss_func 'mse' --scheduler 'steplr'"
-
-    # training from scratch: ovf_coarser
-    "python train_fuse.py --data_dir '/mnt/g/ovf/ovf_superpixel_dataset' --config 'configs/config_ovf_coarser.yaml' --task 'tsc_aligned' --log_name 'tsc_aligned_ovf_coarser' --gpus 1 --batch_size 32 --head 'fuse_head' --dataset 'ovf_coarser' --encoder 'b' --network 'ResUnet' --lr 5e-4 --emb_dims 768 --loss_func 'mse' --scheduler 'steplr'"
-
-    # training from scratch: ovf_4class
-    "python train_fuse.py --data_dir '/mnt/g/ovf/ovf_superpixel_dataset' --config 'configs/config_ovf_4class.yaml' --task 'tsc_aligned' --log_name 'tsc_aligned_ovf_4class' --gpus 1 --batch_size 32 --head 'fuse_head' --dataset 'ovf_4class' --encoder 'b' --network 'ResUnet' --lr 5e-4 --emb_dims 768 --loss_func 'mse' --scheduler 'steplr'"
-
-    # tuning using pre-trained tsc_aligned_rmf_sp
-    "python train_fuse.py --data_dir '/mnt/g/ovf/ovf_superpixel_dataset' --config 'configs/config_ovf_sp.yaml' --task 'tsc_aligned' --log_name 'tsc_aligned_rmf_sp_pretained_ovf_sp_tuned' --gpus 1 --batch_size 32 --head 'fuse_head' --dataset 'ovf_sp' --encoder 'b' --network 'ResUnet' --lr 1e-3 --emb_dims 768 --loss_func 'mse' --scheduler 'steplr' --pretrained_ckpt 'tl_logs/tsc_aligned_ovf_sp/checkpoints/final_model.ckpt'"
-
-    "python train_fuse.py --data_dir '/mnt/g/ovf/ovf_superpixel_dataset' --config 'configs/config_ovf_genus.yaml' --task 'tsc_aligned' --log_name 'tsc_aligned_rmf_sp_pretained_ovf_genus_tuned' --gpus 1 --batch_size 32 --head 'fuse_head' --dataset 'ovf_genus' --encoder 'b' --network 'ResUnet' --lr 1e-3 --emb_dims 768 --loss_func 'mse' --scheduler 'steplr' --pretrained_ckpt 'tl_logs/tsc_aligned_ovf_sp/checkpoints/final_model.ckpt'"
-
-    "python train_fuse.py --data_dir '/mnt/g/ovf/ovf_superpixel_dataset' --config 'configs/config_ovf_coarser.yaml' --task 'tsc_aligned' --log_name 'tsc_aligned_rmf_sp_pretained_ovf_coarser_tuned' --gpus 1 --batch_size 32 --head 'fuse_head' --dataset 'ovf_coarser' --encoder 'b' --network 'ResUnet' --lr 1e-3 --emb_dims 768 --loss_func 'mse' --scheduler 'steplr' --pretrained_ckpt 'tl_logs/tsc_aligned_ovf_sp/checkpoints/final_model.ckpt'"
-
-    # for comparison with ft-full inint
-    "python finetune.py --cfg 'configs/finetune.yaml' --ft_mode 'full_ft' --pretrained_ckpt 'tsc_aligned_ovf_genus/checkpoints/final_model.ckpt'" 
-
-    "python train_fuse.py --data_dir '/mnt/g/ovf/ovf_superpixel_dataset' --config 'configs/config_ovf_4class.yaml' --task 'tsc_aligned' --log_name 'tsc_aligned_rmf_sp_pretained_ovf_4class_tuned' --gpus 1 --batch_size 32 --head 'fuse_head' --dataset 'ovf_4class' --encoder 'b' --network 'ResUnet' --lr 1e-3 --emb_dims 768 --loss_func 'mse' --scheduler 'steplr' --pretrained_ckpt 'tl_logs/tsc_aligned_ovf_sp/checkpoints/final_model.ckpt'"
-
-    # tuning using pre-trained tsc_ResUnet_aligned_noms_pointnext_b_rmf_sp_mse_weighted_normal (+ normal)
-    "python train_fuse.py --data_dir '/mnt/g/ovf/ovf_superpixel_dataset' --config 'configs/config_ovf_sp.yaml' --task 'tsc_aligned' --log_name 'tsc_aligned_rmf_sp_pretained_ovf_sp_normal_tuned' --pc_normal True --gpus 1 --batch_size 32 --head 'fuse_head' --dataset 'ovf_sp' --encoder 'b' --network 'ResUnet' --lr 1e-3 --emb_dims 768 --loss_func 'mse' --scheduler 'steplr' --pretrained_ckpt 'tl_logs/tsc_ResUnet_aligned_noms_pointnext_b_rmf_sp_mse_weighted_normal/checkpoints/final_model.ckpt'"
-
-    "python train_fuse.py --data_dir '/mnt/g/ovf/ovf_superpixel_dataset' --config 'configs/config_ovf_coarser.yaml' --task 'tsc_aligned' --log_name 'tsc_aligned_rmf_sp_pretained_ovf_coarser_normal_tuned' --pc_normal True --gpus 1 --batch_size 32 --head 'fuse_head' --dataset 'ovf_coarser' --encoder 'b' --network 'ResUnet' --lr 1e-3 --emb_dims 768 --loss_func 'mse' --scheduler 'steplr' --pretrained_ckpt 'tl_logs/tsc_ResUnet_aligned_noms_pointnext_b_rmf_sp_mse_weighted_normal/checkpoints/final_model.ckpt'"
-
-    "python train_fuse.py --data_dir '/mnt/g/ovf/ovf_superpixel_dataset' --config 'configs/config_ovf_4class.yaml' --task 'tsc_aligned' --log_name 'tsc_aligned_rmf_sp_pretained_ovf_4class_tuned' --pc_normal True --gpus 1 --batch_size 32 --head 'fuse_head' --dataset 'ovf_4class' --encoder 'b' --network 'ResUnet' --lr 1e-3 --emb_dims 768 --loss_func 'mse' --scheduler 'steplr' --pretrained_ckpt 'tl_logs/tsc_ResUnet_aligned_noms_pointnext_b_rmf_sp_mse_weighted_normal/checkpoints/final_model.ckpt'"
-
-    # pre-training: rmf_genus
-    "python train_fuse.py --data_dir '/mnt/g/rmf/rmf_superpixel_dataset' --config 'configs/config_rmf_genus.yaml' --task 'tsc_aligned' --log_name 'tsc_aligned_rmf_genus' --gpus 1 --batch_size 32 --head 'fuse_head' --dataset 'rmf_genus' --encoder 'b' --network 'ResUnet' --lr 1e-3 --emb_dims 768 --loss_func 'mse' --scheduler 'steplr'"
-
-    # tuning using pre-trained tsc_aligned_rmf_genus
-    "python train_fuse.py --data_dir '/mnt/g/ovf/ovf_superpixel_dataset' --config 'configs/config_ovf_sp.yaml' --task 'tsc_aligned' --log_name 'tsc_aligned_rmf_genus_pretained_ovf_sp_tuned' --gpus 1 --batch_size 32 --head 'fuse_head' --dataset 'ovf_sp' --encoder 'b' --network 'ResUnet' --lr 1e-3 --emb_dims 768 --loss_func 'mse' --scheduler 'steplr' --pretrained_ckpt 'tl_logs/tsc_aligned_rmf_genus/checkpoints/final_model.ckpt'"
-
-    "python train_fuse.py --data_dir '/mnt/g/ovf/ovf_superpixel_dataset' --config 'configs/config_ovf_genus.yaml' --task 'tsc_aligned' --log_name 'tsc_aligned_rmf_genus_pretained_ovf_genus_tuned' --gpus 1 --batch_size 32 --head 'fuse_head' --dataset 'ovf_genus' --encoder 'b' --network 'ResUnet' --lr 1e-3 --emb_dims 768 --loss_func 'mse' --scheduler 'steplr' --pretrained_ckpt 'tl_logs/tsc_aligned_rmf_genus/checkpoints/final_model.ckpt'"
-
-    "python train_fuse.py --data_dir '/mnt/g/ovf/ovf_superpixel_dataset' --config 'configs/config_ovf_coarser.yaml' --task 'tsc_aligned' --log_name 'tsc_aligned_rmf_genus_pretained_ovf_coarser_tuned' --gpus 1 --batch_size 32 --head 'fuse_head' --dataset 'ovf_coarser' --encoder 'b' --network 'ResUnet' --lr 1e-3 --emb_dims 768 --loss_func 'mse' --scheduler 'steplr' --pretrained_ckpt 'tl_logs/tsc_aligned_rmf_genus/checkpoints/final_model.ckpt'"
-
-    "python train_fuse.py --data_dir '/mnt/g/ovf/ovf_superpixel_dataset' --config 'configs/config_ovf_4class.yaml' --task 'tsc_aligned' --log_name 'tsc_aligned_rmf_genus_pretained_ovf_4class_tuned' --gpus 1 --batch_size 32 --head 'fuse_head' --dataset 'ovf_4class' --encoder 'b' --network 'ResUnet' --lr 1e-3 --emb_dims 768 --loss_func 'mse' --scheduler 'steplr' --pretrained_ckpt 'tl_logs/tsc_aligned_rmf_genus/checkpoints/final_model.ckpt'"
-
-    # pre-training: rmf_4class
-    "python train_fuse.py --data_dir '/mnt/g/rmf/rmf_superpixel_dataset' --config 'configs/config_rmf_4class.yaml' --task 'tsc_aligned' --log_name 'tsc_aligned_rmf_4class' --gpus 1 --batch_size 32 --head 'fuse_head' --dataset 'rmf_4class' --encoder 'b' --network 'ResUnet' --lr 1e-3 --emb_dims 768 --loss_func 'mse' --scheduler 'steplr'"
-
-    # tuning using pre-trained tsc_aligned_rmf_4class
-    "python train_fuse.py --data_dir '/mnt/g/ovf/ovf_superpixel_dataset' --config 'configs/config_ovf_sp.yaml' --task 'tsc_aligned' --log_name 'tsc_aligned_rmf_4class_pretained_ovf_sp_tuned' --gpus 1 --batch_size 32 --head 'fuse_head' --dataset 'ovf_sp' --encoder 'b' --network 'ResUnet' --lr 1e-3 --emb_dims 768 --loss_func 'mse' --scheduler 'steplr' --pretrained_ckpt 'tl_logs/tsc_aligned_rmf_4class/checkpoints/final_model.ckpt'"
-
-    "python train_fuse.py --data_dir '/mnt/g/ovf/ovf_superpixel_dataset' --config 'configs/config_ovf_genus.yaml' --task 'tsc_aligned' --log_name 'tsc_aligned_rmf_4class_pretained_ovf_genus_tuned' --gpus 1 --batch_size 32 --head 'fuse_head' --dataset 'ovf_genus' --encoder 'b' --network 'ResUnet' --lr 1e-3 --emb_dims 768 --loss_func 'mse' --scheduler 'steplr' --pretrained_ckpt 'tl_logs/tsc_aligned_rmf_4class/checkpoints/final_model.ckpt'"
-
-    "python train_fuse.py --data_dir '/mnt/g/ovf/ovf_superpixel_dataset' --config 'configs/config_ovf_coarser.yaml' --task 'tsc_aligned' --log_name 'tsc_aligned_rmf_4class_pretained_ovf_coarser_tuned' --gpus 1 --batch_size 32 --head 'fuse_head' --dataset 'ovf_coarser' --encoder 'b' --network 'ResUnet' --lr 1e-3 --emb_dims 768 --loss_func 'mse' --scheduler 'steplr' --pretrained_ckpt 'tl_logs/tsc_aligned_rmf_4class/checkpoints/final_model.ckpt'"
-
-    "python train_fuse.py --data_dir '/mnt/g/ovf/ovf_superpixel_dataset' --config 'configs/config_ovf_4class.yaml' --task 'tsc_aligned' --log_name 'tsc_aligned_rmf_4class_pretained_ovf_4class_tuned' --gpus 1 --batch_size 32 --head 'fuse_head' --dataset 'ovf_4class' --encoder 'b' --network 'ResUnet' --lr 1e-3 --emb_dims 768 --loss_func 'mse' --scheduler 'steplr' --pretrained_ckpt 'tl_logs/tsc_aligned_rmf_4class/checkpoints/final_model.ckpt'"
-
-    # tuning rmf_sp using pretained ovf_sp
-    "python train_fuse.py --data_dir '/mnt/g/rmf/rmf_superpixel_dataset' --config 'configs/config_rmf_sp.yaml' --task 'tsc_aligned' --log_name 'tsc_aligned_ovf_sp_pretained_rmf_sp_tuned' --gpus 1 --batch_size 32 --head 'fuse_head' --dataset 'rmf_sp' --encoder 'b' --network 'ResUnet' --lr 1e-3 --emb_dims 768 --loss_func 'mse' --scheduler 'steplr' --pretrained_ckpt 'tsc_aligned_ovf_sp/checkpoints/final_model.ckpt'"
-
-    # tuning rmf_genus using pretained ovf_sp
-    "python train_fuse.py --data_dir '/mnt/g/rmf/rmf_superpixel_dataset' --config 'configs/config_rmf_genus.yaml' --task 'tsc_aligned' --log_name 'tsc_aligned_ovf_sp_pretained_rmf_genus_tuned' --gpus 1 --batch_size 32 --head 'fuse_head' --dataset 'rmf_genus' --encoder 'b' --network 'ResUnet' --lr 1e-3 --emb_dims 768 --loss_func 'mse' --scheduler 'steplr' --pretrained_ckpt 'tsc_aligned_ovf_sp/checkpoints/final_model.ckpt'"  
-
-    # tuning rmf_4class using pretained ovf_sp
-    "python train_fuse.py --data_dir '/mnt/g/rmf/rmf_superpixel_dataset' --config 'configs/config_rmf_4class.yaml' --task 'tsc_aligned' --log_name 'tsc_aligned_ovf_sp_pretained_rmf_4class_tuned' --gpus 1 --batch_size 32 --head 'fuse_head' --dataset 'rmf_4class' --encoder 'b' --network 'ResUnet' --lr 1e-3 --emb_dims 768 --loss_func 'mse' --scheduler 'steplr' --pretrained_ckpt 'tsc_aligned_ovf_sp/checkpoints/final_model.ckpt'"  
-
-    # tuning rmf_genus using pretained ovf_genus
-    "python train_fuse.py --data_dir '/mnt/g/rmf/rmf_superpixel_dataset' --config 'configs/config_rmf_genus.yaml' --task 'tsc_aligned' --log_name 'tsc_aligned_ovf_genus_pretained_rmf_genus_tuned' --gpus 1 --batch_size 32 --head 'fuse_head' --dataset 'rmf_genus' --encoder 'b' --network 'ResUnet' --lr 1e-3 --emb_dims 768 --loss_func 'mse' --scheduler 'steplr' --pretrained_ckpt 'tsc_aligned_ovf_genus/checkpoints/final_model.ckpt'"  
-
-    # pre-training rmf_sp with ewmse
-    "python train_fuse.py --data_dir '/mnt/g/rmf/rmf_superpixel_dataset' --config 'configs/config_rmf_sp.yaml' --task 'tsc_aligned' --log_name 'tsc_aligned_rmf_sp_ewmse' --gpus 1 --batch_size 32 --head 'fuse_head' --dataset 'rmf_sp' --encoder 'b' --network 'ResUnet' --lr 1e-3 --emb_dims 768 --loss_func 'ewmse' --scheduler 'steplr'"
-
-    "python train_fuse.py --data_dir '/mnt/g/ovf/ovf_superpixel_dataset' --config 'configs/config_ovf_coarser.yaml' --task 'tsc_aligned' --log_name 'tsc_aligned_rmf_sp_ewmse_ovf_coarser_tuned' --gpus 1 --batch_size 32 --head 'fuse_head' --dataset 'ovf_coarser' --encoder 'b' --network 'ResUnet' --lr 1e-3 --emb_dims 768 --loss_func 'mse' --scheduler 'steplr' --pretrained_ckpt 'tl_logs/tsc_aligned_rmf_sp_ewmse/checkpoints/final_model.ckpt'"
-
-    "python train_fuse.py --data_dir '/mnt/g/ovf/ovf_superpixel_dataset' --config 'configs/config_ovf_4class.yaml' --task 'tsc_aligned' --log_name 'tsc_aligned_rmf_sp_ewmse_ovf_4class_tuned' --gpus 1 --batch_size 32 --head 'fuse_head' --dataset 'ovf_4class' --encoder 'b' --network 'ResUnet' --lr 1e-3 --emb_dims 768 --loss_func 'mse' --scheduler 'steplr' --pretrained_ckpt 'tl_logs/tsc_aligned_rmf_sp_ewmse/checkpoints/final_model.ckpt'"
+    "python finetune.py --cfg 'configs/finetune.yaml' --ft_mode 'freeze_last_3' --pretrained_ckpt 'tl_logs/tsc_aligned_rmf_sp/checkpoints/final_model.ckpt'"
+    
     )
 
 # Loop over and run each command
